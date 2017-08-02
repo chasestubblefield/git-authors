aa974ef893d63a0cee8c48cf9fa0a2a1bbc13e37
Chase Stubblefield <chasestubblefield@gmail.com>
2017-08-02 12:32:20 -0700

    Update specs

--- spec/git-authors/config_spec.rb
+++ spec/git-authors/config_spec.rb
@@ -1,56 +1,62 @@
 require 'spec_helper'
 
 describe GitAuthors::Config do
-  let(:config) { GitAuthors::Config.new(gitconfig.path) }
-  let(:gitconfig) { Tempfile.open('gitconfig') }
 
   before :all do
-    gitconfig.write <<-END.gsub(/^ {6}/, '')
+    @gitconfig = Tempfile.open('gitconfig')
+    @config = GitAuthors::Config.new(@gitconfig.path)
+
+    @gitconfig.write <<-END.gsub(/^ {6}/, '')
       [git-authors "foo"]
         base-email = bar@example.com
         bill = Bill Murray
         jack = Jack Nicholson
         john = John Goodman
     END
-    gitconfig.rewind
+    @gitconfig.rewind
   end
 
   after :all do
-    gitconfig.close
-    gitconfig.unlink
+    @gitconfig.close
+    @gitconfig.unlink
   end
 
   describe '.new' do
     it 'accepts the path to a custom config file' do
-      config.instance_variable_get(:@config_path).should == gitconfig.path
+      config_path = @config.instance_variable_get(:@config_path)
+      expect(config_path).to eq(@gitconfig.path)
     end
 
     it 'also accepts no custom config file (will use gitconfig)' do
-      GitAuthors::Config.new.instance_variable_get(:@config_path).should be_nil
+      config_path = GitAuthors::Config.new.instance_variable_get(:@config_path)
+      expect(config_path).to be_nil
     end
   end
 
   describe '#raw_output' do
     it 'should return the output of `git config`' do
-      config.send(:raw_output).should == <<-END.gsub(/^ {8}/, '')
+      raw_output = @config.send(:raw_output)
+      expect(raw_output).to eq(<<-END.gsub(/^ {8}/, '')
         git-authors.foo.base-email bar@example.com
         git-authors.foo.bill Bill Murray
         git-authors.foo.jack Jack Nicholson
         git-authors.foo.john John Goodman
       END
+      )
     end
   end
 
   describe '#to_hash' do
     it 'should return a hash of short names to full names' do
-      config.send(:to_hash).should == {
+      to_hash = @config.send(:to_hash)
+      expect(to_hash).to eq({
         'foo' => {
           'base-email' => 'bar@example.com',
           'bill' => 'Bill Murray',
           'jack' => 'Jack Nicholson',
           'john' => 'John Goodman',
         }
-      }
+      })
     end
   end
 end
